7ae53629adf50bc690cdc0a96c0b9d35c7bfa65f
Marcus McDuffie <marcus.mcduffie@matere.us>
2015-07-05 09:05:40 -0400

    Added a test... Which is failing.

---
+++ app/addToPool.js
@@ -0,0 +1,4 @@
+exports.addToPool = function(objPool,name,parent) {
+  obj = {name: name, parent: parent};
+  objPool.push(obj);
+};
--- test/test.js
+++ test/test.js
@@ -1,6 +1,16 @@
 var assert = require("assert");
+var addToPool = require("../app/addToPool");
+
 describe('universe', function(){
   it('should return true when truth is real', function(){
+    assert.equal(true,true);
+  })
+});
+
+describe('addToPool', function(){
+  it('Object should be in pool after we add it.', function(){
+    var objPool = [];
+    addToPool(objPool,"test1",null);
     assert.equal(true,true);
   })
 });
